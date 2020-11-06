bd67c2c10cfef5067fb270cc6cd8b2bcae5d0e2e
Pau Larrea <paularrea12@hotmail.com>
2020-11-06 16:03:19 +0100

    mobile responsive

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import TradingPairs from "./components/TradingPairs/TradingPairs";
 
 function App() {
   return (
-    <div className="row wrapper m-0">
-      <div className="average col-md-6 col-xs-12 p-0">
-        <AvgTicker />
-      </div>
-      <div className="col-md-6 col-xs-12 p-0">
-        <TradingPairs />
+    <div className='app'>
+      <div className="row wrapper m-0">
+        <div className="average col-md-6 col-xs-12 p-0">
+          <AvgTicker />
+        </div>
+        <div className="col-md-6 col-xs-12 p-0">
+          <TradingPairs />
+        </div>
       </div>
     </div>
   );
